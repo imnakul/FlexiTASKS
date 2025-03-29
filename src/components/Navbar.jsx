44bfd31a1a824f5fdbdaaa7fa3604ebf6257e5e2
Nakul Srivastava <imnakul44@gmail.com>
2025-03-29 19:25:30 +0530

    CheckPoint 3 - Feedback form added , just animated themes + Notion Integration left + Check everything in Mobile view- not responsive many things for Phone view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,9 +50,9 @@ function Navbar() {
             <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
                <div className='flex items-center justify-between h-16'>
                   {/* Logo/Title */}
-                  <div className='flex-shrink-0 flex items-center gap-4'>
+                  <div className='flex-shrink-0 flex items-center gap-3'>
                      <FaTasks className='w-7 h-7 text-white' />
-                     <h1 className='text-2xl font-bold text-gray-800 dark:text-white'>
+                     <h1 className='text-2xl font-bold text-gray-800 dark:text-white sm:pb-0.5 pb-1'>
                         Task Master
                      </h1>
                   </div>
